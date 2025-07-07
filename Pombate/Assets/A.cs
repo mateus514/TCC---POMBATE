485882b5d9b815d4524025d607763104e42ae23a
mateus <145375374+mateus514@users.noreply.github.com>
2025-07-07 17:31:38 -0300

    a
    a

---
+++ Pombate/Assets/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "String Eevent Channel", menuName = "Scriptable Objects/StringEeventChannel")]
+
+public class StringEeventChannel : ScriptableObject
+{
+    public event Action<string> OnEventRaised;
+
+    public void RaiseEvent(string value)
+    {
+        OnEventRaised?.Invoke(value);
+    }
+}
