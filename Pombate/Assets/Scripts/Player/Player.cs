a58f130a728482b05ef20f1c98c3ae1d98c0a122
mateus <145375374+mateus514@users.noreply.github.com>
2025-07-07 21:38:30 -0300

    a
    a

--- Pombate/Assets/Scripts/Player/Player.cs
+++ Pombate/Assets/Scripts/Player/Player.cs
@@ -1,47 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 public class Player : MonoBehaviour
 {
     private float horizontalInput;
     private Rigidbody2D rb;
 
-   [SerializeField] private int velocidade = 5;
-   
+    [SerializeField] private int velocidade = 5;
+    [SerializeField] private float forcaPulo = 600f;
 
-   [SerializeField] private  Transform  peDoPersonagem;
-   [SerializeField] private  LayerMask  chaoLayer;
+    [SerializeField] private Transform peDoPersonagem;
+    [SerializeField] private LayerMask chaoLayer;
 
-   private bool estaNoChao;
+    [SerializeField] private InputActionReference pointerPosition;
 
-   private Animator animator;
-   
-   private int MovendoHash = Animator.StringToHash("Movendo");
-   private int PulandoHash = Animator.StringToHash("Pulando");
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector2 pointerInput;
+
+    private bool estaNoChao;
+
+    private Animator animator;
+
+    private WeaponParent weaponParent;
+
+    private int MovendoHash = Animator.StringToHash("Movendo");
+    private int PulandoHash = Animator.StringToHash("Pulando");
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        weaponParent = GetComponentInChildren<WeaponParent>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        pointerInput = GetPointerInput();
+        weaponParent.PointerPosition = pointerInput;
+
         horizontalInput = Input.GetAxis("Horizontal");
 
         if (Input.GetKeyDown(KeyCode.Space) && estaNoChao)
         {
-            rb.AddForce(Vector2.up * 600);
+            rb.AddForce(Vector2.up * forcaPulo);
         }
-        
+
         estaNoChao = Physics2D.OverlapCircle(peDoPersonagem.position, 0.2f, chaoLayer);
-        animator.SetBool("Movendo", horizontalInput != 0);
-        animator.SetBool("Pulando", !estaNoChao);
+
+        animator.SetBool(MovendoHash, horizontalInput != 0);
+        animator.SetBool(PulandoHash, !estaNoChao);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontalInput * 5, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(horizontalInput * velocidade, rb.linearVelocity.y);
+    }
+
+    private Vector2 GetPointerInput()
+    {
+        Vector3 mousePos = pointerPosition != null
+            ? pointerPosition.action.ReadValue<Vector2>()
+            : Mouse.current.position.ReadValue();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
