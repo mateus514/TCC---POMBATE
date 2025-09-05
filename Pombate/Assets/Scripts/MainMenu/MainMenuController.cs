f997bff42d7679e91f7336d58d1ebea86ad83397
mateus <145375374+mateus514@users.noreply.github.com>
2025-09-05 17:18:52 -0300

    Tomaaa
    - Agora pode sair do jogo clicando em "Sair"!

--- Pombate/Assets/Scripts/MainMenu/MainMenuController.cs
+++ Pombate/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -8,4 +8,12 @@ public class MainMenuController : MonoBehaviour
         // Troca para a cena chamada "Tutorial1"
         SceneManager.LoadScene("Fase1 - Pule e Pule na borda");
     }
+    public void SairDoJogo()
+    {
+        // Fecha o jogo
+        Application.Quit();
+
+        // Debug útil no editor
+        Debug.Log("Sair do jogo chamado!");
+    }
 }
